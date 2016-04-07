c53f5f54e1d73e03590d04a42821667cea0aaf94
xwenzi <419040774@qq.com>
2016-04-08 01:04:57 +0800

    jkljkljlkjlk

---
+++ trdy/src/main/java/com/jd/test/hjkjkljlk.java
@@ -0,0 +1,14 @@
+package com.jd.test;
+
+public class hjkjkljlk {
+
+	public hjkjkljlk() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
