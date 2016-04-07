6f3e6edb93b5948065a67143dbcbf632f7d336c5
xwenzi <419040774@qq.com>
2016-04-08 00:56:56 +0800

    jkljlkjkl

--- trdy/src/main/java/com/jd/test/trtrtr.java
+++ trdy/src/main/java/com/jd/test/trtrtr.java
@@ -4,11 +4,12 @@ public class trtrtr {
 
 	public trtrtr() {
 		// TODO Auto-generated constructor stubhjkhkhjkhjkhkj
+		qianglei
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubhhoiuiouoiuiouoiu
-
+			qianglei
 	}
 
 }
